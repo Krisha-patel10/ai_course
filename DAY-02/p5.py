78ae1c0abfd36a29f0f8fc84f4e50351b522ae24
Krisha Patel <kp5633080@gmail.com>
2026-01-17 07:11:52 +0000

    c

---
+++ DAY-02/p5.py
@@ -0,0 +1,12 @@
+points = {1,44,77,44,33,1}
+print(points)
+print(type(points))
+
+cart = ["pc","chocolate","colddrink","pc","chocolate","shoes"]
+print(cart)
+
+cart_un = set(cart)
+print(cart_un)
+
+cart_li  = list(cart_un)
+print(cart_li)
