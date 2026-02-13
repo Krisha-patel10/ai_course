5107247c8100a5a4651f1fadd7127c47ceb9ff27
Krisha Patel <kp5633080@gmail.com>
2026-02-13 09:05:49 +0530

    Fix total_sales calculation to use sum method

--- DAY-08/Totalsales.py
+++ DAY-08/Totalsales.py
@@ -4,6 +4,6 @@ data = pd.read_csv("coffee_shop_sales.csv")
 data["total_price"] = data["transaction_qty"] * data["unit_price"]
 
 total_sales = 0
-total_sales += data["total_price"]
+total_sales += data["total_price"].sum()
 print(total_sales)
 print(data.head())
