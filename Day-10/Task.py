64aeeb3b61a20624903ad6ccf79c32e8f5eaa849
Krisha Patel <kp5633080@gmail.com>
2026-03-03 10:35:41 +0530

    Fix data processing and plotting in Task.py

--- Day-10/Task.py
+++ Day-10/Task.py
@@ -6,11 +6,14 @@ data = pd.read_csv("coffee_shop_sales.csv")
 data['total_amount'] = data['unit_price'] * data['transaction_qty']
 
 data['transaction_time'] = pd.to_datetime(data['transaction_time'], format='%H:%M:%S')
+data['total_amount'] = data['unit_price'] * data['transaction_qty']
+
+data['transaction_time'] = pd.to_datetime(data['transaction_time'])
 
 data['hour'] = data['transaction_time'].dt.hour
 
 average_sales_per_hour = data.groupby('hour')['total_amount'].mean()
-
+plt.figure()
 plt.plot(average_sales_per_hour.index, average_sales_per_hour.values)
 
 plt.xlabel("Hour of Day")
